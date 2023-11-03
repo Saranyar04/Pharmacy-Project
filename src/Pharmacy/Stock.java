93e070bb29b92acc0fb1400dd9cb80c59a9393b3
Saranyar04 <srajendran.laba@solvd.com>
2023-11-03 00:15:58 -0400

    log

--- src/Pharmacy/Stock.java
+++ src/Pharmacy/Stock.java
@@ -3,16 +3,8 @@
 package Pharmacy;
 
 public class Stock {
-    int stock_item_no;
-    String stock_name;
+    int stockItemNo;
+    String stockName;
     String location;
-    int batch_no;
-
-    public void addStock(int stock_item_no, String stock_name, String location, int batch_no){
-        this.stock_item_no = stock_item_no;
-        this.stock_name = stock_name;
-        this.location = location;
-        this.batch_no = batch_no;
-    }
-    public void getStocknumber() {}
+    int batchNoo;
 }
