59e8f7f207dc450fdf556ac091bce42a60345cf1
Saranyar04 <srajendran.laba@solvd.com>
2023-11-30 01:24:29 -0500

    moved txt file to resource folder

--- src/main/java/uniquewords/UniqueWordReader.java
+++ src/main/java/uniquewords/UniqueWordReader.java
@@ -3,7 +3,7 @@ package uniquewords;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.Logger;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,14 +12,14 @@ import java.util.Set;
 
 public class UniqueWordReader {
 
-    private static final Logger LOGGER = LogManager.getLogger(UniqueWordReader.class);
+    private static final Logger LOGGER = (Logger) LogManager.getLogger(UniqueWordReader.class);
 
     public void uniqueWords() {
-        File file = new File("Description.txt");
-        Set<String> uniqueWords = new HashSet<>( );
+        File file = new File("C:\\Users\\HP\\IdeaProjects\\Pharmacy Project\\src\\main\\resources\\Description.txt");
+        Set<String> uniqueWords = new HashSet<>();
         try {
             String content = FileUtils.readFileToString(file, "UTF-8");
-            content = content.replaceAll("[^a-zA-Z)-9_-]", "");
+            content = content.replaceAll("[,.]", "");
             String[] words = StringUtils.split(content);
             for (int i = 0; i < words.length; i++) {
                 uniqueWords.add(words[i]);
