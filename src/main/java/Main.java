8e9a7a5929dd8063e68097ff59673296432ab826
Saranyar04 <srajendran.laba@solvd.com>
2023-12-08 12:34:02 -0500

    Removing unwanted spaces

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -41,8 +41,8 @@ public class Main {
     private static final Logger LOGGER = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        Pharmacy pharmacy = new Pharmacy( );
-        pharmacy.printInfo( );
+        Pharmacy pharmacy = new Pharmacy();
+        pharmacy.printInfo();
         ReentrantLock firstLock = new ReentrantLock();
 
         synchronized (firstLock) {
@@ -51,7 +51,7 @@ public class Main {
                 Runnable runnable = new RunnableThread("Worker Thread : " + i);
                 executer.execute(runnable);
             }
-            executer.shutdown( );
+            executer.shutdown();
             while (!executer.isTerminated()) {
             }
         }
@@ -64,8 +64,8 @@ public class Main {
                 RunnableThread runnableThread = new RunnableThread("Worker Thread : " + i);
                 executorService.execute(runnableThread);
             }
-            executorService.shutdown( );
-            while (!executorService.isTerminated( )) {
+            executorService.shutdown();
+            while (!executorService.isTerminated()) {
             }
         }
         System.out.println("All requests completed successfully.");
@@ -73,19 +73,19 @@ public class Main {
         ReentrantLock thirdLock = new ReentrantLock();
         synchronized (thirdLock) {
             ExecutorService executorService1 = Executors.newFixedThreadPool(5);
-            List<Future<Integer>> list = new ArrayList<>( );
+            List<Future<Integer>> list = new ArrayList<>();
             for (int i = 0; i < 7; i++) {
                 Future<Integer> future = executorService1.submit(new SumCallable(i));
                 list.add(future);
             }
             for (Future i : list) {
                 try {
-                    LOGGER.info(i.get( ));
+                    LOGGER.info(i.get());
                 } catch (InterruptedException | ExecutionException e) {
                     LOGGER.info(e);
                 }
-                executorService1.shutdown( );
-                while (!executorService1.isTerminated( )) {
+                executorService1.shutdown();
+                while (!executorService1.isTerminated()) {
                 }
             }
         }
@@ -98,44 +98,44 @@ public class Main {
 
             try {
                 Class<?> pharmacy1 = Class.forName("Pharmacy");
-                Constructor<?>[] constructors = pharmacy1.getConstructors( );
+                Constructor<?>[] constructors = pharmacy1.getConstructors();
                 LOGGER.info("Constructors : " + Arrays.toString(constructors));
 
-                Method[] methods = pharmacy1.getMethods( );
+                Method[] methods = pharmacy1.getMethods();
                 LOGGER.info("Methods : " + Arrays.toString(methods));
 
-                Field[] declaredFields = pharmacy1.getDeclaredFields( );
+                Field[] declaredFields = pharmacy1.getDeclaredFields();
                 LOGGER.info("Fields : " + Arrays.toString(declaredFields));
 
                 try {
                     Doctor doctor = new Doctor("Seegol", "Male", new Date("3/12/1989"), "4132458796", "61 Riverview Ave, New Jersey", "seegol@yahoo.com,", "Surgeon");
                     Method adddoctor = pharmacy1.getDeclaredMethod("addDoctor", Doctor.class);
                     adddoctor.setAccessible(true);
-                    adddoctor.invoke(new Pharmacy( ), doctor);
+                    adddoctor.invoke(new Pharmacy(), doctor);
 
                     Employee employee = new Employee("Kim", "Female", new Date("04/02/2000"), "2349887453", "23 Riverview Dr, New Jersey", "kim@gmail.com", "pharmacist", EmployeeType.PHARMACIST, 3000);
                     Method addemployee = pharmacy1.getDeclaredMethod("addEmployee", Employee.class);
                     addemployee.setAccessible(true);
-                    addemployee.invoke(new Pharmacy( ), employee);
+                    addemployee.invoke(new Pharmacy(), employee);
                     Employee employeeDelaina = new Employee("Delaina", "Female", new Date("04/05/1990"), "2349887768", "83 Riverview Dr, New Jersey", "delaina@gmail.com", "pharmacist", EmployeeType.PHARMACIST, 3000);
-                    addemployee.invoke(new Pharmacy( ), employee);
+                    addemployee.invoke(new Pharmacy(), employee);
 
                     Customer lilly = new Customer("Lilly", "Female", new Date("04/11/2020"), "4132246155", "23 Riviera Dr, Main St, NewJersey", "lilly@gmail.com", NO_INSURANCE, doctor);
                     Method addcustomer = pharmacy1.getDeclaredMethod("addCustomer", Customer.class);
                     addcustomer.setAccessible(true);
-                    addcustomer.invoke(new Pharmacy( ), lilly);
+                    addcustomer.invoke(new Pharmacy(), lilly);
                     Customer lilyana = new Customer("Lilyana", "Female", new Date("04/11/2020"), "4132246155", "23 Riviera Dr, Main St, NewJersey", "lilly@gmail.com", PLAN_A, doctor);
-                    addcustomer.invoke(new Pharmacy( ), lilyana);
+                    addcustomer.invoke(new Pharmacy(), lilyana);
 
                     Medicine medicine = new Medicine("M453456", "Paracetamol", MedicineType.OVER_THE_COUNTER, CompanyName.PNC_PHARMACY, "Pain reliever", 12.30);
                     Method addmedicine = pharmacy1.getDeclaredMethod("addMedicine", Medicine.class);
                     addmedicine.setAccessible(true);
-                    addmedicine.invoke(new Pharmacy( ), medicine);
-                    CustomLinkedList<Medicine> customLinkedList = new CustomLinkedList<>( );
+                    addmedicine.invoke(new Pharmacy(), medicine);
+                    CustomLinkedList<Medicine> customLinkedList = new CustomLinkedList<>();
                     customLinkedList.add(medicine);
-                    customLinkedList.print( );
+                    customLinkedList.print();
 
-                    List<Medicine> medicines = new ArrayList<>( );
+                    List<Medicine> medicines = new ArrayList<>();
                     medicines.add(new Medicine("M34528", "Tylenol", MedicineType.PRESCRIPTION, CompanyName.MDC_LABORATORIES, "Generic for age 13+", 23));
                     medicines.add(new Medicine("M34567", "Paracetamol", MedicineType.OVER_THE_COUNTER, CompanyName.TI_LAB, "for ages 15+", 13));
 
@@ -146,15 +146,15 @@ public class Main {
 
                     pharmacy.calculateSale(employee, lilly, medicines);
                     pharmacy.calculateSale(employeeDelaina, lilyana, medicines);
-                    pharmacy.printPersonMap( );
-                    pharmacy.printPrescriptionQueue( );
+                    pharmacy.printPersonMap();
+                    pharmacy.printPrescriptionQueue();
                     print(doctor);
-                    pharmacy.printReceipts( );
+                    pharmacy.printReceipts();
                     pharmacy.getCustomerReceipts("Lilly");
-                    pharmacy.viewCompanyMedicineList( );
-                    pharmacy.viewMedicineNames( );
-                    pharmacy.printSaleEmployeeList( );
-                    pharmacy.printSortedReceiptTotal( );
+                    pharmacy.viewCompanyMedicineList();
+                    pharmacy.viewMedicineNames();
+                    pharmacy.printSaleEmployeeList();
+                    pharmacy.printSortedReceiptTotal();
                 } catch (InvalidPriceException | InvalidMedicineListException |
                          InvalidLegalNameException | InvalidPhoneNoException |
                          NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
@@ -163,7 +163,7 @@ public class Main {
             } catch (ClassNotFoundException e) {
                 throw new RuntimeException(e);
             }
-            UniqueWordReader uniqueWordReader = new UniqueWordReader( );
+            UniqueWordReader uniqueWordReader = new UniqueWordReader();
             uniqueWordReader.countUniqueWords("pharmacy.txt");
         }
     }
